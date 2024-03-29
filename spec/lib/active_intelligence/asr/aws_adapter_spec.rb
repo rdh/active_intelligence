e331fce9968878763db5b93a3da9c11ccef42c52
Rich Humphrey <rich.humphrey@masterclass.com>
2024-03-29 11:40:28 -0700

    Tweaked `Prompable#from_llm` to take an adapter name from config, rather than object reference

--- spec/lib/active_intelligence/asr/aws_adapter_spec.rb
+++ spec/lib/active_intelligence/asr/aws_adapter_spec.rb
@@ -14,8 +14,9 @@ describe ActiveIntelligence::ASR::AwsAdapter do
     let(:json) { JSON.parse(File.read(path), symbolize_names: true) }
     let(:path) { 'spec/data/audio/podcast/aws_diarization.json' }
 
-    it 'returns a string' do
-      expect(subject).to be_a(String)
+    it 'returns am array of strings' do
+      expect(subject).to be_an(Array)
+      expect(subject).to all(be_a(String))
     end
   end
 end
